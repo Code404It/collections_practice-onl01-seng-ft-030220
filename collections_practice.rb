4eb24591d292522aee58299cebbc726b73630573
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-15 02:15:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,6 +40,12 @@ def sum_array(array)
   array.sum
 end  
 
-def add_s(array)
- if 
+ def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
 end
